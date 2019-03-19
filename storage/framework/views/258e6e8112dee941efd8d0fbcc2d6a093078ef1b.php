fa8e9dea1d5bd884236b1b8392ebc9dfbf2b4a61
Lilian Berna <arphax33@gmail.com>
2019-03-19 09:20:28 +0100

    sync commit

---
+++ storage/framework/views/258e6e8112dee941efd8d0fbcc2d6a093078ef1b.php
@@ -0,0 +1,23 @@
+<aside class="main-sidebar" id="sidebar-wrapper">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Recherche..."/>
+          <span class="input-group-btn">
+            <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+            </button>
+          </span>
+            </div>
+        </form>
+        <!-- Sidebar Menu -->
+
+        <ul class="sidebar-menu" data-widget="tree">
+            <?php echo $__env->make('layouts.menu', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        </ul>
+        <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
