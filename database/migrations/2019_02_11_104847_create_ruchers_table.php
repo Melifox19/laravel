8de625e1adb6cce7164ebbeecb111653d437d764
Lilian Berna <arphax33@gmail.com>
2019-04-06 18:34:30 +0200

    correction bug migrations

--- database/migrations/2019_02_11_104847_create_ruchers_table.php
+++ database/migrations/2019_02_11_104847_create_ruchers_table.php
@@ -19,9 +19,8 @@ class CreateRuchersTable extends Migration
             $table->integer('idApiculteur')->unsigned();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
-            $table->timestamp('deleted_at')->nullable();
             $table->softDeletes();
-            $table->foreign('idApiculteur')->references('id')->on('users');  
+            $table->foreign('idApiculteur')->references('id')->on('users');
         });
     }
 
--- database/migrations/2019_02_11_143917_create_alertes_table.php
+++ database/migrations/2019_02_11_143917_create_alertes_table.php
@@ -21,7 +21,6 @@ class CreateAlertesTable extends Migration
             $table->integer('idRuche')->unsigned();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
-            $table->timestamp('deleted_at')->nullable();
             $table->softDeletes();
             $table->foreign('idRuche')->references('id')->on('ruches');
         });
