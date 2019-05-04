587092e131cd9c4a248c9037f1844f2cb0c2ac27
Lilian Berna <arphax33@gmail.com>
2019-05-04 19:56:16 +0200

    Api MaJ 5 (status: Meliruche sending working and inserting)

--- app/Http/Controllers/API/MesureAPIController.php
+++ app/Http/Controllers/API/MesureAPIController.php
@@ -37,128 +37,120 @@ class MesureAPIController extends AppBaseController
 
             $ruche = Ruche::where('addrMelinet', $data['addrMelinet'])->where('idMeliborne', $meliborne->id)->first();
 
-            return response()->json($ruche->id, 201);
-
             $mesure = Mesure::create([
                 'horodatageMesure' => $data['horodatageMesure'],
                 'masse' => $data['masse'],
                 'temperatureInt' => $data['temperatureInt'],
                 'temperatureExt' => $data['temperatureExt'],
                 'humiditeInt' => $data['humiditeInt'],
-                'humiditeExt' => 'NULL',
                 'pression' => $data['pression'],
                 'niveauBatterie' => $data['niveauBatterie'],
-                'debitSonore200' => 'NULL',
-                'debitSonore400' => 'NULL',
                 'idRuche' => $ruche->id
             ]);
 
-            return response()->json($mesure, 201);
-
-
             // On vérifie si les valeurs ne sont pas trop critiques pour pouvoir créer l'alerte
 
-            if ($attributs->masse > 70) // Si la masse est supérieure à 70 kg
+            if ($mesure->masse > 70) // Si la masse est supérieure à 70 kg
             {
-                Alerte::insert([
+                $alerte = $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Masse elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->masse < 20)
+            if ($mesure->masse < 20)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Masse faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
 
             }
-            if ($attributs->masse < 0)
+            if ($mesure->masse < 0)
             {
-                Alert::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'vol',
                     'description' => 'Ruche non detectee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ------------ Température intérieure ---------------
 
-            if ($attributs->temperatureInt > 36)
+            if ($mesure->temperatureInt > 36)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature interieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->temperatureInt < 30)
+            if ($mesure->temperatureInt < 30)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature interieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // --------- Température extérieure --------
 
-            if ($attributs->temperatureExt > 40)
+            if ($mesure->temperatureExt > 40)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature exterieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->temperatureExt < 0)
+            if ($mesure->temperatureExt < 0)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature exterieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ----------------- Humidité intérieure -----------------
 
-            if ($attributs->humiditeInt > 25)
+            if ($mesure->humiditeInt > 25)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Humidite interieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->humiditeInt < 20)
+            if ($mesure->humiditeInt < 20)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Humidite interieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ----------- Niveau de batterie --------
 
-            if ($attributs->niveauBatterie < 30)
+            if ($mesure->niveauBatterie < 30)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Batterie faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
@@ -173,7 +165,7 @@ class MesureAPIController extends AppBaseController
 
             $ruche = Ruche::where('idSigfox', $idSigfox);
 
-            $attributs = [
+            $mesure = [
                 'horodatageMesure' => $data['horodatageMesure'],
                 'masse' => $data['masse'],
                 'temperatureInt' => $data['temperatureInt'],
@@ -187,156 +179,156 @@ class MesureAPIController extends AppBaseController
                 'idRuche' => $ruche->id
             ];
             // on crée un article à partir du tableau d'atributs
-            $mesure = Mesure::create($attributs);
+            $mesure = Mesure::create($mesure);
 
 
             // On vérifie si les valeurs ne sont pas trop critiques pour pouvoir créer l'alerte
 
-            if ($attributs->masse > 70) // Si la masse est supérieure à 70 kg
+            if ($mesure->masse > 70) // Si la masse est supérieure à 70 kg
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Masse elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->masse < 20)
+            if ($mesure->masse < 20)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Masse faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
 
             }
-            if ($attributs->masse < 0)
+            if ($mesure->masse < 0)
             {
-                Alert::insert([
+                Alert::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'vol',
                     'description' => 'Ruche non detectee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ------------ Température intérieure -------------------
 
-            if ($attributs->temperatureInt > 36)
+            if ($mesure->temperatureInt > 36)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature interieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->temperatureInt < 30)
+            if ($mesure->temperatureInt < 30)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature interieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ------------- Température extérieure ---------------
 
-            if ($attributs->temperatureExt > 40)
+            if ($mesure->temperatureExt > 40)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature exterieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->temperatureExt < 0)
+            if ($mesure->temperatureExt < 0)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Temperature exterieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ------------- Humidité intérieure ---------------------
 
-            if ($attributs->humiditeInt > 25)
+            if ($mesure->humiditeInt > 25)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Humidite interieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->humiditeInt < 20)
+            if ($mesure->humiditeInt < 20)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Humidite interieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
 
             // ------------- Humidité intérieure ------------------
 
-            if ($attributs->humiditeExt > 25)
+            if ($mesure->humiditeExt > 25)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Humidite exterieure elevee',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
-            if ($attributs->humiditeExt < 20)
+            if ($mesure->humiditeExt < 20)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Humidite exterieure faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // -------------- Niveau de batterie -------------
 
-            if ($attributs->niveauBatterie < 30)
+            if ($mesure->niveauBatterie < 30)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Batterie faible',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
             // ------------ Débit Sonore (200Hz & 400 Hz) --------------
 
-            if ($attributs->debitSonore200 > 190 && $attributs->debitSonore200 < 210)
+            if ($mesure->debitSonore200 > 190 && $mesure->debitSonore200 < 210)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Essaimage potentiel (200 Hz)',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
-            if ($attributs->debitSonore400 > 400 && $attributs->debitSonore400 < 500)
+            if ($mesure->debitSonore400 > 400 && $mesure->debitSonore400 < 500)
             {
-                Alerte::insert([
+                $alerte = Alerte::create([
                     'horodatageAlerte' => date("Y-m-d H:i:s"),
                     'type' => 'mesure',
                     'description' => 'Essaimage potentiel (400 Hz)',
-                    'idRuche' => $attributs->idRuche
+                    'idRuche' => $mesure->idRuche
                 ]);
             }
 
@@ -348,13 +340,13 @@ class MesureAPIController extends AppBaseController
             $idSigfox = $data['idSigfox'];
             $ruche = Ruche::where('idSigfox', $idSigfox);
 
-            $attributs = [
+            $mesure = [
                 'longitude' => $data['longitude'],
                 'latitude' => $data['latitude'],
                 'idRuche' => $ruche->id
             ];
 
-            $mesure = Mesure::create($attributs);
+            $mesure = Mesure::create($mesure);
             // on retourne l'article créé et un code réponse 201 (created)
             return response()->json($mesure, 201);
             break;
