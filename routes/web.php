2f7717db290f6edfdb292ec1be0b644055604ec6
Lilian Berna <arphax33@gmail.com>
2019-05-21 09:27:14 +0200

    route minor bug correction

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,6 @@ Route::group(['middleware' => ['auth', 'verified']],function()
   Route::group(['middleware' => ['admin']], function()
   {
     Route::resource('users', 'UserController');
-    Route::resource('mesures', 'MesureController');
-    Route::resource('alertes', 'AlerteController');
   });
 
   // Route pour tout les utilisateurs ----------------------------------------------------
@@ -45,4 +43,8 @@ Route::group(['middleware' => ['auth', 'verified']],function()
   Route::resource('ruches', 'RucheController');
 
   Route::resource('home', 'HomeController');
+
+  Route::resource('mesures', 'MesureController');
+  
+  Route::resource('alertes', 'AlerteController');
 });
